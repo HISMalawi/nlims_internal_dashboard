a9bc1d22c827a896955fe98df8e5d2ded2b09207
hopgausi <hopgausi20@gmail.com>
2021-06-09 08:16:57 +0200

    date filter implemented

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,55 +1,72 @@
-class HomeController < ApplicationController
+require 'date'
 
-    def index 
-        data = {
-            sites: ['ekwendeni', 'chiwavi', 'area 28']
-        }
-        # render json: data
+class HomeController < ApplicationController
+    def index
+        current_time = DateTime.now
+        year = current_time.strftime "%Y"
+        month = current_time.strftime "%m"
+        month_year = Date::MONTHNAMES[month.to_i] + ', '+ year
+        # year = params[:yearMonth].split('-')[0]
+        # month = params[:yearMonth].split('-')[1]
+
+        # puts('year '+ year + ' month '+ month)
     end
 
     def qech
+        year = params[:year]
+        month = params[:month]
+        month_year = Date::MONTHNAMES[month.to_i]
         data = {
             total_orders_submitted:350, total_orders_accepted: 300,
             total_orders_rejected: 50, total_tests: 1000,
             total_tests_verrified: 800, total_tests_with_results:700,
             total_tests_rejected: 100, total_tests_waiting_results: 50,
-            total_tests_to_be_started: 70, timeline: 'June 2017',
+            total_tests_to_be_started: 70, timeline: month_year,
             sites: ['ekwendeni', 'chiwavi', 'area 28','ekwandeni', 'chiwevi', 'area 18']
         }
         render json: data
     end
 
     def kch
+        year = params[:year]
+        month = params[:month]
+        month_year = Date::MONTHNAMES[month.to_i]
         data = {
             total_orders_submitted:33350, total_orders_accepted: 33300,
             total_orders_rejected: 3350, total_tests: 331000,
             total_tests_verrified: 33800, total_tests_with_results:33700,
             total_tests_rejected: 33100, total_tests_waiting_results: 3350,
-            total_tests_to_be_started: 3370, timeline: 'June 2017',
+            total_tests_to_be_started: 3370, timeline: month_year,
             sites: ['ekwendeni', 'chiwavi', 'area 28','ekwandeni', 'chiwevi', 'area 18']
         }
         render json: data
     end
 
     def mzuzuch
+        year = params[:year]
+        month = params[:month]
+        month_year = Date::MONTHNAMES[month.to_i]
         data = {
             total_orders_submitted:11350, total_orders_accepted: 11300,
             total_orders_rejected: 1150, total_tests: 111000,
             total_tests_verrified: 11800, total_tests_with_results:11700,
             total_tests_rejected: 11100, total_tests_waiting_results: 1150,
-            total_tests_to_be_started: 1170, timeline: 'June 2017',
+            total_tests_to_be_started: 1170, timeline: month_year,
             sites: ['ekwendeni', 'chiwavi', 'area 28','ekwandeni', 'chiwevi', 'area 18']
         }
         render json: data
     end
 
     def mzimbadh
+        year = params[:year]
+        month = params[:month]
+        month_year = Date::MONTHNAMES[month.to_i]
         data = {
             total_orders_submitted:88350, total_orders_accepted: 88300,
             total_orders_rejected: 8850, total_tests: 881000,
             total_tests_verrified: 88800, total_tests_with_results:88700,
             total_tests_rejected: 88100, total_tests_waiting_results: 8850,
-            total_tests_to_be_started: 8870, timeline: 'June 2017',
+            total_tests_to_be_started: 8870, timeline: month_year,
             sites: ['ekwendeni', 'chiwavi', 'area 28','ekwandeni', 'chiwevi', 'area 18']
         }
         render json: data
@@ -57,13 +74,16 @@ class HomeController < ApplicationController
 
 
     def genexpert
+        year = params[:year]
+        month = params[:month]
+        month_year = Date::MONTHNAMES[month.to_i]
         if params[:hospital] == 'ekwendeni'
         data = {
             total_orders_submitted:22350, total_orders_accepted: 22300,
             total_orders_rejected: 2250, total_tests: 221000,
             total_tests_verrified: 22800, total_tests_with_results:22700,
             total_tests_rejected: 22100, total_tests_waiting_results: 2250,
-            total_tests_to_be_started: 2270, timeline: 'June 2017',
+            total_tests_to_be_started: 2270, timeline: month_year,
             sites: ['ekwendeni', 'chiwavi', 'area 28','ekwandeni', 'chiwevi', 'area 18']
         }
         render json: data
@@ -75,7 +95,7 @@ class HomeController < ApplicationController
                 total_orders_rejected: 111150, total_tests: 11111000,
                 total_tests_verrified: 1111800, total_tests_with_results:1111700,
                 total_tests_rejected: 1111100, total_tests_waiting_results: 111150,
-                total_tests_to_be_started: 111170, timeline: 'June 2017',
+                total_tests_to_be_started: 111170, timeline: month_year,
                 sites: ['ekwendeni', 'chiwavi', 'area 28','ekwandeni', 'chiwevi', 'area 18']
             }
             render json: data
@@ -86,7 +106,7 @@ class HomeController < ApplicationController
                 total_orders_rejected: 100150, total_tests: 10011000,
                 total_tests_verrified: 1001800, total_tests_with_results:1001700,
                 total_tests_rejected: 1001100, total_tests_waiting_results: 100150,
-                total_tests_to_be_started: 100170, timeline: 'June 2017',
+                total_tests_to_be_started: 100170, timeline: month_year,
                 sites: ['ekwendeni', 'chiwavi', 'area 28','ekwandeni', 'chiwevi', 'area 18']
             }
             render json: data
