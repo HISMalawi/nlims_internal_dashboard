ac4f86df7e850751b72ae9dfb98cacade3cb7e71
hopgausi <hopgausi20@gmail.com>
2021-06-15 09:29:57 +0200

    implemented today's stats feature

--- public/javascripts/scripts/lab.js
+++ public/javascripts/scripts/lab.js
@@ -1,15 +1,20 @@
 var hospital
 var period;
 
+$('.text-muted').css('display', 'none');
 // ajax call function
 function ajaxCall(uri, color) {
     let selector = uri.replace("/query_lab_stats_", "").trim().split('?')[0].split('_').join('-');
     jQuery.ajax({
         url: uri,
         type: "Post",
+        dataType: "json",
         success: function(res) {
-            $(`#${selector}`).text(res);
+            $('.text-muted').css('display', 'initial');
+            $(`#${selector}`).text(res.data);
+            $(`.${selector}`).text(res.today);
             $(`#${selector}`).css('color', `${color}`);
+            console.log($(`#${selector}`));
         },
         error: function(err) {
             console.log(err);
@@ -59,12 +64,13 @@ function setHospital(lab) {
 
 
 function loadData(lab) {
-    parameters = `lab_name=${lab}&period=${period}`
-        // Set hospital name
+    // Set hospital name
     setHospital(lab);
 
     // Get filter date
     getSetDate();
+
+    parameters = `lab_name=${lab}&period=${period}`
 
     // css style for header and filter
     $('.header').css('display', '');
@@ -77,6 +83,9 @@ function loadData(lab) {
     // total orders accepted ajax call
     url = `/query_lab_stats_total_orders_accepted?${parameters}`;
     ajaxCall(url, 'green');
+
+    url = `/query_lab_stats_total_orders_to_be_accepted?${parameters}`;
+    ajaxCall(url, '#99a364');
 
     // total orders rejected ajax call
     url = `/query_lab_stats_total_orders_rejected?${parameters}`;
@@ -105,6 +114,10 @@ function loadData(lab) {
     // total tests to be started
     url = `/query_lab_stats_total_tests_to_be_started?${parameters}`;
     ajaxCall(url, '#99a364');
+
+    // total tests to be started
+    url = `/query_lab_stats_total_tests_voided_failed?${parameters}`;
+    ajaxCall(url, 'red');
 
     // last sync ajax call
     url = `/query_lab_stats_last_sync?${parameters}`;
