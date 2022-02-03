2d8c27439a056addf25f4700fb6a453014fef994
Hopson Gausi <hopgausi20@gmail.com>
2022-02-03 11:26:00 +0200

    Rails logger config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+Rails.application.configure do
+    Rails.logger = Logger.new(STDOUT)
+    config.logger = ActiveSupport::Logger.new("log/check_sync_status.log")
+end
