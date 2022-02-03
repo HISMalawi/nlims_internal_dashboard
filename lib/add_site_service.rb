835da3d27e42f597ab10f9cf9e29d53ff3127fee
Hopson Gausi <hopgausi20@gmail.com>
2022-02-03 11:27:45 +0200

    fix add new site and rename host address to ipaddress

--- lib/add_site_service.rb
+++ lib/add_site_service.rb
@@ -1,18 +1,34 @@
 module AddSiteService
-    # file_sites = JSON.parse(File.read(Rails.root.join('public','labs.json')))
     def self.seed_sites(sites)
         sites.each do |key, value|
             site = Site.where(name: key).take
             if site.nil?
-                Site.create(name: key, host_address: value[0], site_code: value[1], x: value[3], y: value[2], district: value[4], region: value[5], enabled: 1)
+                Site.create(name: key, ip_address: value[0], site_code: value[1], x: value[3], y: value[2], district: value[4], region: value[5], enabled: 1)
             else
-                site.update(host_address: value[0], site_code: value[1], enabled: 1)
+                site.update(ip_address: value[0], site_code: value[1], enabled: 1)
             end
         end
     end
 
     def self.add_new_site(params)
-        Site.create(params)
+        if params[:enabled]
+            enable = 1
+        else
+            enable = 0
+        end
+        site = Site.where(name: params[:site_name]).take
+        if site.nil?
+            Site.create(name: params[:site_name].titleize, ip_address: params[:ip_address], site_code: params[:site_code].upcase, 
+                x: params[:longitude], y: params[:latitude], district: params[:district].capitalize, region: params[:region].capitalize, 
+                description: params[:description].titleize,enabled: enable)
+        elsif site[:site_code] == params[:site_code]
+            site.update(ip_address: params[:ip_address],enabled: enable)
+        else 
+            Site.create(name: params[:site_name].titleize, ip_address: params[:ip_address], site_code: params[:site_code].upcase, 
+                x: params[:longitude], y: params[:latitude], district: params[:district].capitalize, region: params[:region].capitalize, 
+                description: params[:description].titleize,enabled: enable)     
+        end
+
     end
 
     def self.update_site(params, id)
