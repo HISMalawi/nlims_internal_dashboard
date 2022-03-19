a2326f0afd57be41d8231e4cc7fa9856c81f1242
Hopson Gausi <hopgausi20@gmail.com>
2022-03-19 22:24:28 +0200

    Maintain Model naming

--- lib/add_site_service.rb
+++ lib/add_site_service.rb
@@ -3,9 +3,9 @@ module AddSiteService
         sites.each do |key, value|
             site = Site.where(name: key).take
             if site.nil?
-                Site.create(name: key, ip_address: value[0], site_code: value[1], x: value[3], y: value[2], district: value[4], region: value[5], enabled: 1)
+                Site.create(name: key, host_address: value[0], site_code: value[1], x: value[3], y: value[2], district: value[4], region: value[5], enabled: 1)
             else
-                site.update(ip_address: value[0], site_code: value[1], enabled: 1)
+                site.update(host_address: value[0], site_code: value[1], enabled: 1)
             end
         end
     end
@@ -18,13 +18,13 @@ module AddSiteService
         end
         site = Site.where(name: params[:site_name]).take
         if site.nil?
-            Site.create(name: params[:site_name].titleize, ip_address: params[:ip_address], site_code: params[:site_code].upcase, 
+            Site.create(name: params[:site_name].titleize, host_address: params[:host_address], site_code: params[:site_code].upcase, 
                 x: params[:longitude], y: params[:latitude], district: params[:district].capitalize, region: params[:region].capitalize, 
                 description: params[:description].titleize,enabled: enable)
         elsif site[:site_code] == params[:site_code]
-            site.update(ip_address: params[:ip_address],enabled: enable)
+            site.update(host_address: params[:host_address],enabled: enable)
         else 
-            Site.create(name: params[:site_name].titleize, ip_address: params[:ip_address], site_code: params[:site_code].upcase, 
+            Site.create(name: params[:site_name].titleize, host_address: params[:host_address], site_code: params[:site_code].upcase, 
                 x: params[:longitude], y: params[:latitude], district: params[:district].capitalize, region: params[:region].capitalize, 
                 description: params[:description].titleize,enabled: enable)     
         end
