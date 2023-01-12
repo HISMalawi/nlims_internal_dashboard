fe7801c51048bd5cb520ef58e081228723c1fc0d
Hopson Gausi <hopgausi20@gmail.com>
2023-01-12 09:05:07 +0200

    Rename functional names

--- app/services/r4h_dashboard/query_builder/acknowledge_result.rb
+++ app/services/r4h_dashboard/query_builder/acknowledge_result.rb
@@ -2,7 +2,7 @@ module R4hDashboard
   module QueryBuilder
     module AcknowledgeResult
       class << self 
-        def count_all(start_date: R4hDashboard::Utils::General.start_date, end_date: R4hDashboard::Utils::General.end_date)
+        def total_count(start_date: R4hDashboard::Utils::General.start_date, end_date: R4hDashboard::Utils::General.end_date)
           central_hospitals = R4hDashboard::Utils::General.central_hospitals
           sql = "SELECT t.test_result_receipent_types as id, trrt.name, COUNT(t.test_result_receipent_types) AS count FROM specimen sp 
             INNER JOIN tests t ON t.specimen_id = sp.id INNER JOIN test_types tt ON tt.id = t.test_type_id
@@ -14,7 +14,7 @@ module R4hDashboard
           sql << " GROUP BY t.test_result_receipent_types"
         end
 
-        def count_per_site(start_date: R4hDashboard::Utils::General.start_date, end_date: R4hDashboard::Utils::General.end_date)
+        def total_count_per_site(start_date: R4hDashboard::Utils::General.start_date, end_date: R4hDashboard::Utils::General.end_date)
           central_hospitals = R4hDashboard::Utils::General.central_hospitals
           sql = "SELECT t.test_result_receipent_types as id, trrt.name, sp.sending_facility, MAX(sp.district) AS district,
             COUNT(t.test_result_receipent_types) AS count
@@ -27,10 +27,10 @@ module R4hDashboard
           sql << " GROUP BY t.test_result_receipent_types,sp.sending_facility"
         end
 
-        def drill_details(site_name: '', start_date: R4hDashboard::Utils::General.start_date, 
+        def drilldown(site_name: '', start_date: R4hDashboard::Utils::General.start_date, 
           end_date: R4hDashboard::Utils::General.end_date, acknowledgement_type: 2)
           central_hospitals = R4hDashboard::Utils::General.central_hospitals
-          sql = "SELECT sp.id AS id, sp.tracking_number,t.test_result_receipent_types AS acknoledgemet_type, sp.sending_facility AS facility,
+          sql = "SELECT sp.id AS id, sp.tracking_number,t.test_result_receipent_types AS acknoledgemet_type, sp.sending_facility,
                  sp.district,  sp.date_created, tt.name AS test_type, t.date_result_given AS emrack_date 
                  FROM specimen sp INNER JOIN tests t ON t.specimen_id = sp.id INNER JOIN test_types tt ON tt.id = t.test_type_id
                 INNER JOIN test_results tr ON tr.test_id = t.id WHERE (tt.name = 'Viral Load' OR tt.name = 'Early Infant Diagnosis')
