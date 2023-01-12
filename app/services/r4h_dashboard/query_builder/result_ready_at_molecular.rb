43a80d14edaedfef59d187408267a2d4c1fc8c8d
Hopson Gausi <hopgausi20@gmail.com>
2023-01-12 10:56:34 +0200

    Fixed result ready query

--- app/services/r4h_dashboard/query_builder/result_ready_at_molecular.rb
+++ app/services/r4h_dashboard/query_builder/result_ready_at_molecular.rb
@@ -4,13 +4,12 @@ module R4hDashboard
       class << self
         def total_count(start_date: R4hDashboard::Utils::General.start_date, end_date: R4hDashboard::Utils::General.end_date)
           central_hospitals = R4hDashboard::Utils::General.central_hospitals
-          sql = "SELECT sdt.id AS id, 'results_ready_at_molecular' AS 'name', COUNT(t.id) AS count FROM specimen_dispatches sd
-            INNER JOIN specimen_dispatch_types sdt ON sdt.id = sd.dispatcher_type_id 
+          sql = "SELECT sd.id AS id, 'results_ready_at_molecular' AS 'name', COUNT(t.id) AS count FROM specimen_dispatches sd
             INNER JOIN specimen sp ON sp.tracking_number = sd.tracking_number
             INNER JOIN tests t ON t.specimen_id = sp.id INNER JOIN test_types tt ON tt.id = t.test_type_id 
             INNER JOIN test_results tr ON tr.test_id = t.id 
             WHERE (tt.name='Viral Load' OR tt.name='Early Infant Diagnosis') AND sp.priority = 'Routine' AND
-            sdt.id = 1 AND (tr.result <> '' OR tr.result IS NOT NULL)"
+            sd.dispatcher_type_id = 1 AND (tr.result <> '' OR tr.result IS NOT NULL)"
           sql << " AND sp.sending_facility NOT IN #{central_hospitals}"
           sql << " AND (substr(sd.created_at,1,10) BETWEEN '#{start_date}' AND '#{end_date}')"
         end
