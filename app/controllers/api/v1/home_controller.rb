301b25e1b18e59fa8d824ea9103e54be8ed75093
Hopson Gausi <hopgausi20@gmail.com>
2023-01-24 11:56:28 +0200

    Refactored home controller

--- app/controllers/api/v1/home_controller.rb
+++ app/controllers/api/v1/home_controller.rb
@@ -1,19 +1,41 @@
 class Api::V1::HomeController < ApplicationController
   before_action :set_params
-  def index
+  def orders
     today = HomeDashboard::Utils::General.today
     obj = {
-      cumulative: {
-        orders: HomeDashboard::Orders.all(start_date: @params[:start_date], end_date: @params[:end_date], testtype: @params[:testtype], facility_code: @params[:facility_code]),
-        tests: HomeDashboard::Tests.all(start_date: @params[:start_date], end_date: @params[:end_date], testtype: @params[:testtype], facility_code: @params[:facility_code])
-      },
-      today: {
-        orders: HomeDashboard::Orders.all(start_date: today, end_date: today, testtype: @params[:testtype], facility_code: @params[:facility_code]),
-        tests: HomeDashboard::Tests.all(start_date: today, end_date: today, testtype: @params[:testtype], facility_code: @params[:facility_code])
-      },
-      last_sync: HomeDashboard::Utils::LastSyncDate.get(@params[:facility_code]),
-      test_types: HomeDashboard::Utils::TestTypes.all,
+      cumulative: HomeDashboard::Orders.all(start_date: @params[:start_date], end_date: @params[:end_date], testtype: @params[:testtype], facility_code: @params[:facility_code]),
+      today: HomeDashboard::Orders.all(start_date: today, end_date: today, testtype: @params[:testtype], facility_code: @params[:facility_code])
+
+    }
+    render json: obj
+  end
+
+  def tests
+    today = HomeDashboard::Utils::General.today
+    obj = {
+      cumulative: HomeDashboard::Tests.all(start_date: @params[:start_date], end_date: @params[:end_date], testtype: @params[:testtype], facility_code: @params[:facility_code]),
+      today: HomeDashboard::Tests.all(start_date: today, end_date: today, testtype: @params[:testtype], facility_code: @params[:facility_code])
+    }
+    render json: obj
+  end 
+
+  def sites 
+    obj = {
       sites: HomeDashboard::Utils::General.sites
+    }
+    render json: obj
+  end
+
+  def test_types
+    obj = {
+      test_types: HomeDashboard::Utils::TestTypes.all
+    }
+    render json: obj
+  end
+
+  def last_sync_date
+    obj = {
+      last_sync_date: HomeDashboard::Utils::LastSyncDate.get(@params[:facility_code])
     }
     render json: obj
   end
